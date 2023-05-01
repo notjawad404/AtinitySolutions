43d024dfe0e0d806c4fc16da078216e0b9ea4aac
notjawad404 <jawadali000123@gmail.com>
2023-05-01 18:29:18 +0500

    Updated project

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import About from './components/main/About'
 import Contact from './components/main/Contact';
 import Services from './components/main/Services';
 
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <>
-      {/* <Home/> */}
-      {/* <About/> */}
-      <Services/>
-      {/* <Contact/> */}
+    <Router>
+      <Routes>
+        <Route path='/Home' element={<Home/>} />
+        <Route path='/About' element= {<About/>}/>
+        <Route path='/Contact' element={<Contact/>} />
+        <Route path='/Services' element={<Services/>}/>
+      </Routes>
+    </Router>
     </>
   );
 }
